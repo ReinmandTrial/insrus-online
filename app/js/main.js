271e2797d4ee74e3d663701fc52c933a89e6500f
spjustice <sanyok.vergun@gmail.com>
2021-10-07 19:46:50 +0300

    add page login

--- app/js/main.js
+++ app/js/main.js
@@ -1,79 +1,121 @@
 
 
-$(document).ready(function(){
+$(document).ready(function () {
    var _Seconds;
 
    //ВВод в инпут номер страхового полиса 
-   $('.js-input-track').on('keyup',function(){
+   $('.js-input-track').on('keyup', function () {
       var val = $(this).val();
-      if(val != ''){
-         $('.js-btn-track').prop('disabled',false);
-      }else{
-         $('.js-btn-track').prop('disabled',true);
+      if (val != '') {
+         $('.js-btn-track').prop('disabled', false);
+      } else {
+         $('.js-btn-track').prop('disabled', true);
       }
-      });
+   });
    //ВВод в инпут номер страхового полиса конец
 
-   $('.js-btn-track').on('click',function(){
+   $('.js-btn-track').on('click', function () {
       var btn = $(this);
       var block = btn.closest('.policy-num');
-      if(block.find('input.input').val() != ''){
+      if (block.find('input.input').val() != '') {
          block.addClass('success');
-         if(btn.hasClass('input__search-icon')){
+         if (btn.hasClass('input__search-icon')) {
             $('.detailed-info').removeClass('open');
-            $('.js-btn-details').prop('disabled',false);
+            $('.js-btn-details').prop('disabled', false);
             $('.js-input-pasport').val('');
-            $('.js-btn-popup').prop('disabled',true);
+            $('.js-btn-popup').prop('disabled', true);
             $('.polis-info').removeClass('open');
          }
       }
-   })
+   });
 
-   $('.js-btn-search').on('click',function(){
-      $('.js-btn-track').prop('disabled',true);
-      $('.js-btn-popup').prop('disabled',true);
-      $('.js-btn-details').prop('disabled',false);
+   $('.js-btn-search').on('click', function () {
+      $('.js-btn-track').prop('disabled', true);
+      $('.js-btn-popup').prop('disabled', true);
+      $('.js-btn-details').prop('disabled', false);
       $('.detailed-info').removeClass('open');
       $('.detailed-info').find('input.input').val('');
       $('.polis-info').removeClass('open');
-   })
+   });
 
 
-   $('.js-btn-details').on('click',function(){
+   $('.js-btn-details').on('click', function () {
       $('.detailed-info').addClass('open');
-      $(this).prop('disabled',true);
-   })
+      $(this).prop('disabled', true);
+   });
 
    //ВВод в инпут номер паспорта
-   $('.js-input-pasport').on('keyup',function(){
+   $('.js-input-pasport').on('keyup', function () {
       var val = $(this).val();
-      if(val != ''){
-         $('.js-btn-popup').prop('disabled',false);
-      }else{
-         $('.js-btn-popup').prop('disabled',true);
+      if (val != '') {
+         $('.js-btn-popup').prop('disabled', false);
+      } else {
+         $('.js-btn-popup').prop('disabled', true);
       }
-      });
+   });
    //ВВод в инпут номер паспорта конец
 
+   //Ввод в инпут логин
+   $('.js-input-login').on('keyup', function () {
+      var valLogin = $('.js-input-log').val();
+      var valPass = $('.js-input-pass').val();
+      if (valLogin != '' && valPass != '') {
+         $('.js-btn-log').prop('disabled', false);
+      } else {
+         $('.js-btn-log').prop('disabled', true);
+      }
+   });
+   //ВВод в инпут логин конец
+
+   //Ввод в инпут логин через номер
+   $('.js-input-login-phone-num').on('keyup', function () {
+      var val = $(this).val();
+      if (val != '') {
+         $('.js-btn-login-phone-num').prop('disabled', false);
+      } else {
+         $('.js-btn-login-phone-num').prop('disabled', true);
+      }
+   });
+   //ВВод в инпут логин через номер конец
+
+   // открыть блок в вводом смс
+   $('.js-btn-login-phone-num').on('click', function () {
+      var btn = $(this);
+      $(this).closest('.tab-phone-num__num-block').hide();
+      btn.closest('.tab-phone-num__body').find('.tab-phone-num__sms-block').show();
+   });
+   // открыть блок в вводом смс конец
+
+   //Ввод в инпут код подтверждения
+   $('.js-input-sms').on('keyup', function () {
+      var val = $(this).val();
+      if (val != '') {
+         $('.js-btn-sms').prop('disabled', false);
+      } else {
+         $('.js-btn-sms').prop('disabled', true);
+      }
+   });
+   //Ввод в инпут код подтверждения конец
+
    //открыть попап
-   $('.js-btn-popup').on('click',function(){
+   $('.js-btn-popup').on('click', function () {
       var btn = $(this);
       var input = btn.closest('.detailed-info__container').find('input.input');
-      if(input.val() != ''){
-         $('.js-btn-sms').prop('disabled',true);
+      if (input.val() != '') {
+         $('.js-btn-sms').prop('disabled', true);
          $('.popup-sms').show().addClass('open');
          _Seconds = 30;
          $('.popup-sms__text span').text(_Seconds);
          timer(_Seconds);
       }
-   })
+   });
    //открыть попап конец
 
    //закрыть попап
-   $('.js-popup-close').on('click',function(){
+   $('.js-popup-close').on('click', function () {
       $(this).closest('.popup').hide().removeClass('open');
       $(this).closest('.popup').find('input.js-input-code').val('');
-   })
+   });
    //закрыть попап конец
 
    //нажатие вне попапа
@@ -87,47 +129,81 @@ $(document).ready(function(){
    //нажатие вне попапа конец
 
    //ВВод в инпут кода с смс
-   $('.js-input-code').on('keyup',function(){
+   $('.js-input-code').on('keyup', function () {
       var val = $(this).val();
-      if(val != ''){
-         $('.js-btn-sms').prop('disabled',false);
-      }else{
-         $('.js-btn-sms').prop('disabled',true);
+      if (val != '') {
+         $('.js-btn-sms').prop('disabled', false);
+      } else {
+         $('.js-btn-sms').prop('disabled', true);
       }
-    });
+   });
    //Ввлд в инпут кода с смс конец
 
    //получить информацию
-   $('.js-btn-sms').on('click',function(){
+   $('.js-btn-sms').on('click', function () {
       $(this).closest('.popup').hide().removeClass('open');
       $(this).closest('.popup').find('input.js-input-code').val('');
       $('.detailed-info').removeClass('open');
       $('.polis-info').addClass('open');
-   })
+   });
    //получить информацию конец
+
+   // табы start
+
+   $('.js-tab-trigger').click(function () {
+      var id = $(this).attr('data-tab'),
+         content = $('.js-tab-content[data-tab="' + id + '"]');
+
+      $('.js-tab-trigger.active').removeClass('active');
+      $(this).addClass('active');
+
+      $('.js-tab-content.active').removeClass('active');
+      content.addClass('active');
+   });
+
+   // табы end
 
 })
 
 
 
 // timer
-function timer(_Seconds){
-   if($('.popup-sms').hasClass('open')){
-      if(_Seconds == 0)
-      {
+function timer(_Seconds) {
+   if ($('.popup-sms').hasClass('open')) {
+      if (_Seconds == 0) {
          //  alert('End!');
       } else {
-         setTimeout(function()
-         {
+         setTimeout(function () {
             _Seconds--;
             $('.popup-sms__text span').text(_Seconds);
             timer(_Seconds);
          }, 1000);
       }
-   }else{
+   } else {
       $('.popup-sms__text span').text(30);
       return;
    }
 
 }
 //timer end
+
+// маска для ввода номера
+
+$.fn.setCursorPosition = function (pos) {
+   if ($(this).get(0).setSelectionRange) {
+      $(this).get(0).setSelectionRange(pos, pos);
+   } else if ($(this).get(0).createTextRange) {
+      var range = $(this).get(0).createTextRange();
+      range.collapse(true);
+      range.moveEnd('character', pos);
+      range.moveStart('character', pos);
+      range.select();
+   }
+};
+
+
+$("#phone").click(function () {
+   $(this).setCursorPosition(3);
+}).mask("+380 (99) 999-9999");
+
+// маска для ввода номера конец
